fb51067a6fdb1b21121c65ec25d24d436c92a3cd
omazinov <osman.mazinov@arsis.ru>
2019-10-11 16:25:52 +0300

    Sqlite data provider added

--- DAL/DataBaseContext.cs
+++ DAL/DataBaseContext.cs
@@ -1,13 +1,35 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
 
 namespace DAL
 {
     internal class DataBaseContext : DbContext
     {
-        public DataBaseContext() : base("usefulConnection")
+
+        #region MSSQL Context
+        //public DataBaseContext() : base("usefulConnection")
+        //{
+        //}
+
+        //public DbSet<NoteEntity> Notes { get; set; }
+        #endregion
+
+        /// <summary>
+        /// sqlite3 usefulapp.dp "" Creating empty sqlite database
+        /// </summary>
+        public DataBaseContext() :
+            base(new SQLiteConnection()
+            {
+                ConnectionString = new SQLiteConnectionStringBuilder() { DataSource = @"D:\OpenSourceDevelopment\UsefulApp\NodeService\usefulapp.db", ForeignKeys = true }.ConnectionString
+            }, true)
         {
         }
-
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
         public DbSet<NoteEntity> Notes { get; set; }
     }
 }
